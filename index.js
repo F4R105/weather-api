4fd60a0988ce1f918a2d38a72b02c4bade9a33b6
F4R105 <kajubufaraji@gmail.com>
2023-12-12 18:46:34 +0300

    add more weather data and validate country

--- index.js
+++ index.js
@@ -7,11 +7,27 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+const getSunriseAndSunsetTimes = (timezone, sunrise, sunset) => {
+  const sunrise_time = new Date((sunrise + timezone) * 1000);
+  let sunset_time = new Date((sunset + timezone) * 1000);
+
+  return {
+    sunrise: sunrise_time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+    sunset: sunset_time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+  }
+}
+
 app.get('/', async (req, res) => {
   const{lat,lon} = req.query
   try{
     const weatherRes = await fetch(`${process.env.BASE_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.API_KEY}`)
-    const {weather, main, name} = await weatherRes.json()
+    const weatherData = await weatherRes.json()
+
+    const {weather, main, name, sys, timezone, wind} = weatherData
+    const {country, sunrise, sunset} = sys
+
+    if(country !== "TZ") return res.status(406).json({ message: "Country not supported"})
+
     res.status(200).json({
       name, 
       temp: parseInt(main.temp), 
@@ -20,7 +36,10 @@ app.get('/', async (req, res) => {
           description: weatherItem.description, 
           icon: `${process.env.ICON_URL}/${weather[0].icon}@2x.png`
         })
-      )
+      ),
+      humidity: main.humidity,
+      wind,
+      sun: getSunriseAndSunsetTimes(timezone, sunrise, sunset)
     })
   }catch(error) {
     res.status(500).json({message: error.message})
